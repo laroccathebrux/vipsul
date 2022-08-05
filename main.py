cef08053e59e8f4221369622f744cc8d33b0655e
larocca_thebrux <larocca_thebrux@yahoo.com.br>
2022-08-04 23:48:27 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from datetime import date
+
+def getJson(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    return text
+
+API_ENDPOINT = "https://app.neocrm.com.br/producao-painel-integration"
+today = date.today()
+
+data = { 
+       "tokenEstrutura":"f9d1b75c-fd66-4367-baa8-e09cbb1e478a",
+       "tokenUsuario":"282159ff-bcc4-4cfe-b441-d0dfe79c1848",
+       "dataInicial":"2022-01-01",
+       "dataFinal":str(today),
+       "painelId":"14017"
+    }
+
+r = requests.post(url = API_ENDPOINT, json = data)
+#print(r.text)
+
+f = open('data.csv', "w")
+f.write(r.text)
+f.close()
+
